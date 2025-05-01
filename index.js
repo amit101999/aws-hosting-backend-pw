c6cd194f37ef9e916bf023d9f73352d78fb3f03b
amit from linux machine <amitthakur10sep@gmail.com>
2025-05-01 13:13:58 +0530

    added env

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(process.env.PORT, () => {
-    console.log(`server started ${process.env.PORT}`)
+    console.log(`server started at Port ${process.env.PORT}`)
 })
